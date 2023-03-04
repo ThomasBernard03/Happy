02ad692fb2c08358c17798269585d9718a5a8175
Thomas Bernard <thomas.bernard.310@gmail.com>
2023-03-04 18:22:24 +0100

    support all requests

--- src/models/request.interface.ts
+++ src/models/request.interface.ts
@@ -1,8 +1,10 @@
+import { RequestMethod } from "./enums/request-method"
+
 export interface Request {
     guid : string
     projectGuid : string
     name : string
-    method : string
+    method : RequestMethod
     url : string
     body : string
 }
