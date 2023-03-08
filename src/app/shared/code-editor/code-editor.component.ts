559157609ecc6a101f4a160194c7e0dc695ef05a
Thomas Bernard <90254070+ThomasBernardPageUp@users.noreply.github.com>
2023-03-08 21:02:32 +0100

    add result json readonly

--- src/app/shared/code-editor/code-editor.component.ts
+++ src/app/shared/code-editor/code-editor.component.ts
@@ -1,16 +1,35 @@
-import { Component, Input, DoCheck, Output, EventEmitter } from '@angular/core';
+import { Component, Input, DoCheck, Output, EventEmitter, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-code-editor',
   templateUrl: './code-editor.component.html',
   styleUrls: ['./code-editor.component.scss']
 })
-export class CodeEditorComponent implements DoCheck {
+export class CodeEditorComponent implements DoCheck, OnInit {
 
-  @Input() content! : string
+  @Input() readonly: boolean = false
+
+  @Input() content!: string
   @Output() contentChange = new EventEmitter()
 
-  ngDoCheck() : void {
+  options: any
+
+  ngOnInit(): void {
+    this.options = {
+      readOnly: this.readonly,
+      lineNumbers: true,
+      theme: 'material',
+      height: 'auto',
+      mode: {
+        name: 'javascript',
+        json: true,
+        statementIndent: 2
+      },
+      lineWrapping: true
+    }
+  }
+
+  ngDoCheck(): void {
     this.contentChange.next(this.content);
   }
 
