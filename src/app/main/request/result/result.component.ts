fb4f4a4a7e47b395b8ff6acc357cfdaef9b534e9
Thomas Bernard <90254070+ThomasBernardPageUp@users.noreply.github.com>
2023-03-08 20:48:07 +0100

    display pretty date

--- src/app/main/request/result/result.component.ts
+++ src/app/main/request/result/result.component.ts
@@ -9,19 +9,28 @@ import { RequestService } from 'src/providers/request.service';
 })
 export class ResultComponent implements OnInit {
 
-  constructor(private requestService : RequestService){}
+  constructor(private requestService: RequestService) { }
 
-  result : Result | null = null
+  result: Result | null = null
   formatedDate = ""
 
 
   ngOnInit(): void {
 
     this.requestService.selectedRequest$.subscribe(request => {
-      if(request?.result){
+      if (request?.result) {
         this.result = request?.result
 
-        this.formatedDate = new Date(request.result?.date).toDateString()
+        const date = new Date(request.result?.date)
+
+        let day = date.getDate().toString().padStart(2, '0'); // jour sur 2 chiffres
+        let month = (date.getMonth() + 1).toString().padStart(2, '0'); // mois sur 2 chiffres (attention, les mois commencent à 0 en JavaScript)
+        let year = date.getFullYear(); // année sur 4 chiffres
+        let hours = date.getHours().toString().padStart(2, '0'); // heures sur 2 chiffres
+        let minutes = date.getMinutes().toString().padStart(2, '0'); // minutes sur 2 chiffres
+        let seconds = date.getSeconds().toString().padStart(2, '0'); // secondes sur 2 chiffres
+
+        this.formatedDate = day + '/' + month + '/' + year + ' ' + hours + ':' + minutes + ':' + seconds;
       }
     })
 
