2be92678fba2efbf9d5b520d5b7f84dbccc03d3a
Thomas Bernard <thomas.bernard.310@gmail.com>
2023-03-14 22:18:15 +0100

    display custom select for methods

--- src/app/main/request/request-bar/request-bar.component.ts
+++ src/app/main/request/request-bar/request-bar.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { RequestMethod } from 'src/models/enums/request-method';
+import { MatDialog } from '@angular/material/dialog';
+import { SelectMethodDialogComponent } from './select-method-dialog/select-method-dialog.component';
 
 @Component({
   selector: 'app-request-bar',
@@ -8,16 +10,33 @@ import { RequestMethod } from 'src/models/enums/request-method';
 })
 export class RequestBarComponent {
 
+  constructor(private dialog : MatDialog){}
+
   @Input() method = RequestMethod.Get
-
   @Input() url = ""
-
   @Output() onSendRequest = new EventEmitter()
 
   onSendButtonClicked(){
     this.onSendRequest.emit({
       url: this.url,
       method : this.method
+    })
+  }
+
+  onMethodClicked(){
+    var selectPostion = document.getElementById("request-method-select")?.getBoundingClientRect()
+
+    const instance = this.dialog.open(SelectMethodDialogComponent, {
+      position : {
+        left : selectPostion?.left + "px",
+        top : selectPostion?.top + "px"
+      }
+    })
+
+    instance.afterClosed().subscribe(method => {
+      if(method){
+        this.method = method
+      }
     })
   }
 }
