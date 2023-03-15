c56c52e4e5a009db812f07c2fe0d53e325e4d75c
Thomas Bernard <90254070+ThomasBernardPageUp@users.noreply.github.com>
2023-03-15 22:02:18 +0100

    change code miror

--- src/models/result.interface.ts
+++ src/models/result.interface.ts
@@ -1,12 +1,12 @@
 import { Header } from "./header.interface"
 
-export interface Result {
-    guid : string
-    requestGuid : string
-    code : number
-    status : string
-    body : string
-    headers : Array<Header>,
-    date : number,
-    time : number
+export class Result {
+    guid : string = ""
+    requestGuid : string = ""
+    code : number = 0
+    status : string = ""
+    body : string = ""
+    headers : Array<Header> = []
+    date : number = 0
+    time : number = 0
 }
