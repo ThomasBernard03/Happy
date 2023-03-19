2fd223be936e49f79b36cb9b4ec97930664717a0
Thomas Bernard <thomas.bernard.310@gmail.com>
2023-03-19 22:01:44 +0100

    can get access token

--- src/providers/oauth.service.ts
+++ src/providers/oauth.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core'
 import { ElectronService } from './electron.service';
 
@@ -6,18 +7,38 @@ import { ElectronService } from './electron.service';
 })
 export class OAuthService {
 
-    constructor(private electronService : ElectronService){}
+    constructor(private electronService : ElectronService, private httpClient : HttpClient){}
 
-    githubUrl = "https://github.com/login/oauth/authorize?scope=user:email&client_id=31cd5198b88069a6c76d"
+
+    private clientSecret = " "
+    private githubUrl = `https://github.com/login/oauth/authorize?scope=read:user&client_id=`
+    private clientId = " "
 
 
     getGrantCode(){
 
-        this.electronService.shell?.openExternal(this.githubUrl)
+        this.electronService.shell?.openExternal(`${this.githubUrl}${this.clientId}`)
 
         this.electronService.ipcRenderer?.once("on-login", (e, args) => {
             const grant = args.replace("happy://?code=", "")
-            console.log("grant code : " + grant);
+            console.log("grant code : " + grant)
+
+            this.getAccessToken(grant)
+        })
+    }
+
+    getAccessToken(code : string){
+        const tokenUrl = 'https://github.com/login/oauth/access_token'
+        const body = {
+          client_id: this.clientId,
+          client_secret: this.clientSecret,
+          code: code
+        }
+        const headers = { 'Content-Type': 'application/json', 'Accept': 'application/json' }
+
+        this.httpClient.post(tokenUrl, body, {headers}).subscribe(result => {
+            console.log(result);
+            
         })
     }
 }
